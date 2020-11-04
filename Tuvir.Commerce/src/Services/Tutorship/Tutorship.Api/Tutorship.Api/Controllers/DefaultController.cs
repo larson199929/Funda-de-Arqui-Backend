39ff76bc338d55b1446c89041cb5e1734472fbb4
larson199929 <63831897+larson199929@users.noreply.github.com>
2020-11-03 23:52:07 -0500

    Add files via upload

---
+++ Tuvir.Commerce/src/Services/Tutorship/Tutorship.Api/Tutorship.Api/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorship.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Running...";
+        }
+
+
+
+    }
+}
