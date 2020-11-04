532c8ef13b1ffc0ccc413bda5365f2ea3becb0ad
SanchezD1998 <44213289+SanchezD1998@users.noreply.github.com>
2020-11-04 02:34:54 -0500

    Customer.Service.Queries

---
+++ Customer.Service.Queries/DTOs/ParentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Customer.Service.Queries.DTOs
+{
+    public class ParentDto
+    {
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long Dni { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
