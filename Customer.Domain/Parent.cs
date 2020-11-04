b011043f40a41daabb37d265e9cb15c2d3ea8f94
SanchezD1998 <44213289+SanchezD1998@users.noreply.github.com>
2020-11-04 02:31:45 -0500

    Customer.Domain

---
+++ Customer.Domain/Parent.cs
@@ -0,0 +1,13 @@
+﻿namespace Customer.Domain
+{
+    public class Parent
+    {
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long Dni { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
