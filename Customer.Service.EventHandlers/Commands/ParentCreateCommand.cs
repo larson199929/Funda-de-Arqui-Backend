a02f74e3b9b35d0aea9af5d121abf7da5c711c48
SanchezD1998 <44213289+SanchezD1998@users.noreply.github.com>
2020-11-04 02:33:45 -0500

    Customer.Service.EventHandlers

---
+++ Customer.Service.EventHandlers/Commands/ParentCreateCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Customer.Service.EventHandlers.Commands
+{
+    public class ParentCreateCommand : INotification
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long Dni { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
---
+++ Customer.Service.EventHandlers/ParentEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Customer.Domain;
+using Customer.Persistence.Database;
+using Customer.Service.EventHandlers.Commands;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Customer.Service.EventHandlers
+{
+    public class ParentEventHandler :
+        INotificationHandler<ParentCreateCommand>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ParentEventHandler(
+            ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(ParentCreateCommand notification, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(new Parent {
+                Name = notification.Name
+            });
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
